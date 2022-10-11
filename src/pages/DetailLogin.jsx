36f72f7353629d33a74650626dbf11374cbf36be
khairul umam <khairulumam950@gmail.com>
2022-10-11 09:49:54 +0700

    haii

--- src/pages/DetailLogin.jsx
+++ src/pages/DetailLogin.jsx
@@ -15,11 +15,6 @@ function DetailLogin() {
   });
 
   
-  // let today = new Date();
-
-  // let date = today.getDate() + "-" + parseInt(today.getMonth() + 1) + "-" + today.getFullYear();
-
-
   return (
     <>
       <NavbarUser />
